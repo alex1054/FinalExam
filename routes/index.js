68abbf8ced5fe8705984723a8788d065925eb672
alex1054 <73649290+alex1054@users.noreply.github.com>
2021-12-11 13:04:36 -0500

    Connecting to dictionary api

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ const User = require('../models/user')
 
 // Getting all
 router.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index', { name: 'Alex' })
 })
 // Creating one
 router.get('/login', (req, res) => {
-    res.render('login.ejs')
+    res.render('login')
 })
 
 router.post('/login', (req, res) => {
@@ -17,11 +17,23 @@ router.post('/login', (req, res) => {
 })
 
 router.get('/register', (req, res) => {
-
+    res.render('register')
 })
 
 router.post('/register', (req, res) => {
 
+})
+
+router.get('/search', (req, res) => {
+    const word = req.body.word
+    const data = req.body.data
+    res.render('search', { defWord: word, def: data })
+})
+
+router.post('/search', (req, res) => {
+    const word = req.body.word
+    const data = req.body.data
+    res.render('search', { defWord: word, def: data.shortdef })
 })
 
 module.exports = router
